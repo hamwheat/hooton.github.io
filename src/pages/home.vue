3f68d71e05d02411aaf072a14983b4f24feb5ea9
杨皓天 <yanghaotian@quansantai.com>
2025-01-02 11:03:31 +0800

    style

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,33 +2,40 @@
  * @Author: yang
  * @Date: 2024-12-31 14:41:49
  * @LastEditors: yang
- * @LastEditTime: 2025-01-02 10:58:38
+ * @LastEditTime: 2025-01-02 11:03:26
  * @Description: 
  -->
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
-    <button @click="toggleTheme">Toggle Theme</button>
+    <button @click="toggleTheme" class="theme-toggle">Toggle Theme</button>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Welcome to My Personal Website!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <section class="content">
+      <h1>About Me</h1>
+      <p>This is a brief introduction about myself.</p>
+    </section>
+    <section class="content">
+      <h1>Projects</h1>
+      <p>Here are some of my projects.</p>
+    </section>
   </main>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import HelloWorld from '../components/HelloWorld.vue';
-import TheWelcome from '../components/TheWelcome.vue';
 
 const isDarkTheme = ref(false);
 
 const toggleTheme = () => {
   isDarkTheme.value = !isDarkTheme.value;
   document.documentElement.setAttribute('data-theme', isDarkTheme.value ? 'dark' : 'light');
+  localStorage.setItem('theme', isDarkTheme.value ? 'dark' : 'light');
 };
 
 onMounted(() => {
@@ -45,42 +52,103 @@ onMounted(() => {
 :root {
   --background-color: #ffffff;
   --text-color: #000000;
+  --primary-color: #42b983;
 }
 
 [data-theme="dark"] {
   --background-color: #121212;
   --text-color: #ffffff;
+  --primary-color: #4caf50;
 }
 
 body {
   background-color: var(--background-color);
   color: var(--text-color);
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
 }
 
 header {
-  line-height: 1.5;
+  text-align: center;
+  padding: 1rem;
+  background-color: var(--primary-color);
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  margin-bottom: 1rem;
+}
+
+.theme-toggle {
+  background-color: transparent;
+  border: none;
+  color: var(--text-color);
+  font-size: 1rem;
+  cursor: pointer;
+  margin-bottom: 1rem;
+}
+
+.theme-toggle:hover {
+  text-decoration: underline;
+}
+
+.wrapper {
+  margin-bottom: 1rem;
+}
+
+main {
+  padding: 1rem;
+}
+
+.content {
+  margin-bottom: 2rem;
+}
+
+.content h1 {
+  color: var(--primary-color);
+}
+
+@media (min-width: 768px) {
+  header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .theme-toggle {
+    margin-bottom: 0;
+    margin-top: 1rem;
+  }
+
+  .wrapper {
+    margin-bottom: 2rem;
+  }
+
+  main {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .content {
+    flex: 1;
+    margin: 0 1rem;
+  }
 }
 
 @media (min-width: 1024px) {
   header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .theme-toggle {
+    margin-left: 1rem;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .wrapper {
+    margin-left: 1rem;
   }
 }
 </style>
