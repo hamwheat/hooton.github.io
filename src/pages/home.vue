824938548a9ef21f7b71b693a87d0036c2398d14
杨皓天 <yanghaotian@quansantai.com>
2025-01-02 10:59:10 +0800

    style

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,13 +2,13 @@
  * @Author: yang
  * @Date: 2024-12-31 14:41:49
  * @LastEditors: yang
- * @LastEditTime: 2024-12-31 15:30:55
+ * @LastEditTime: 2025-01-02 10:58:38
  * @Description: 
  -->
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
-
+    <button @click="toggleTheme">Toggle Theme</button>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
@@ -20,12 +20,43 @@
 </template>
 
 <script setup>
-import HelloWorld from '../components/HelloWorld.vue'
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue';
+import HelloWorld from '../components/HelloWorld.vue';
+import TheWelcome from '../components/TheWelcome.vue';
 
+const isDarkTheme = ref(false);
+
+const toggleTheme = () => {
+  isDarkTheme.value = !isDarkTheme.value;
+  document.documentElement.setAttribute('data-theme', isDarkTheme.value ? 'dark' : 'light');
+};
+
+onMounted(() => {
+  // 从localStorage加载主题设置
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    isDarkTheme.value = true;
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+});
 </script>
 
 <style scoped>
+:root {
+  --background-color: #ffffff;
+  --text-color: #000000;
+}
+
+[data-theme="dark"] {
+  --background-color: #121212;
+  --text-color: #ffffff;
+}
+
+body {
+  background-color: var(--background-color);
+  color: var(--text-color);
+}
+
 header {
   line-height: 1.5;
 }
